1dc5aff43a9915d969a0dea608c57a399a871a13
sincsar <sebsibefasil@gmail.com>
2015-03-06 01:21:01 -0600

    Update Web Application

--- src/com/aurora/crms/controller/ApplicationController.java
+++ src/com/aurora/crms/controller/ApplicationController.java
@@ -2,13 +2,16 @@ package com.aurora.crms.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @RequestMapping("/")
+@SessionAttributes("User")
 public class ApplicationController {
 	
 	@RequestMapping("/")
 	public String init(){
-		return "index";
+		return "Welcome";
 	}
+	
 }
